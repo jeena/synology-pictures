fbeefcc01118e0ba52a37dd2d84fae3f1ef2f33b
Jeena <hello@jeena.net>
2023-07-03 10:58:10 +0900

    Add Aaron

--- run-matrix.sh
+++ run-matrix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PATH=$HOME/.local/bin/:$PATH
-pipenv run python matrix.py -n "yingfen|richard|kaylee" -e dongyong -r "\!iyDYVdSookjYKhoXuU:jeena.net"
-pipenv run python matrix.py -n "kinga|lili|karl|richard|georg|christine" -e dongyong -r "\!aPbeAzniGnemrkBcVU:jeena.net"
+pipenv run python matrix.py -n "yingfen|richard|kaylee|aaron" -e dongyong -r "\!iyDYVdSookjYKhoXuU:jeena.net"
+pipenv run python matrix.py -n "kinga|lili|karl|richard|georg|christine|aaron" -e dongyong -r "\!aPbeAzniGnemrkBcVU:jeena.net"
